eef10907c4a593fa5cdd6b354d69eacc30ee07b0
Sadurska <aleksandra.sadurska@capgemini.com>
2021-08-10 16:54:08 +0200

    Initial commit.

---
+++ src/test/java/com/example/welcomeclient/WelcomeClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.welcomeclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomeClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
