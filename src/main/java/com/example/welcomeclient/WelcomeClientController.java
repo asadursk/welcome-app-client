e7842d11166a31c450dd5aa7be400a45a0532b80
asadursk <aleksandra.sadurska@capgemini.com>
2021-08-16 12:05:18 +0200

    refactor(WelcomeClientController): Remove @Autowired annotation and add @AllArgsConstructor

--- src/main/java/com/example/welcomeclient/WelcomeClientController.java
+++ src/main/java/com/example/welcomeclient/WelcomeClientController.java
@@ -1,6 +1,6 @@
 package com.example.welcomeclient;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/call")
+@AllArgsConstructor
 public class WelcomeClientController {
 
-    @Autowired
     private WelcomeClientService welcomeClientService;
 
     @GetMapping("/boss")
