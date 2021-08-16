4da1702423f3f0af9457451aadd0641fd0c4cf2b
asadursk <aleksandra.sadurska@capgemini.com>
2021-08-16 12:18:18 +0200

    refactor(WelcomeClientService): Adjust responsibility range of service. Remove unused @Service annotation and add description.

--- src/main/java/com/example/welcomeclient/WelcomeClientService.java
+++ src/main/java/com/example/welcomeclient/WelcomeClientService.java
@@ -1,12 +1,8 @@
 package com.example.welcomeclient;
 
-import org.springframework.stereotype.Service;
-
 /**
- * WelcomeClientService should communicate with Welcome Application via HTTP client.
- * Each method should invoke proper request from Welcome Application.
+ * WelcomeClientService should use CallService and make a call with proper arguments.
  */
-@Service
 public interface WelcomeClientService {
 
     String callYourBoss(String name);
--- src/main/java/com/example/welcomeclient/WelcomeClientServiceImpl.java
+++ src/main/java/com/example/welcomeclient/WelcomeClientServiceImpl.java
@@ -1,41 +1,27 @@
 package com.example.welcomeclient;
 
-import com.example.welcomeclient.RestTemplate.RestTemplateService;
+import com.example.welcomeclient.CallService.CallService;
 import com.example.welcomeclient.RussianRoulette.RussianRouletteService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+import lombok.AllArgsConstructor;
 
-@Service
+@AllArgsConstructor
 public class WelcomeClientServiceImpl implements WelcomeClientService {
 
-    @Value("${url.colleague}")
-    public String urlToCallColleague;
+    private CallService callService;
 
-    @Value("${url.boss}")
-    public String urlToCallBoss;
-
-    @Autowired
     private RussianRouletteService russianRouletteService;
 
-    @Autowired
-    private RestTemplateService restTemplateService;
-
     public String callYourBoss(String name) {
-        return restTemplateService.makeRequest(urlToCallBoss, name);
+        return callService.callByName(true, name);
     }
 
     public String callYourColleague(String name) {
-        return restTemplateService.makeRequest(urlToCallColleague, name);
+        return callService.callByName(false, name);
     }
 
     public String callSomebody() {
         var isBossSelected = russianRouletteService.spinAndGetResult();
 
-        return restTemplateService.makeRequest(getProperUrl(isBossSelected), "");
-    }
-
-    private String getProperUrl(boolean isBossSelected) {
-        return isBossSelected ? urlToCallBoss : urlToCallColleague;
+        return callService.call(isBossSelected);
     }
 }
